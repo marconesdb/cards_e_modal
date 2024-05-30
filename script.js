048ea48b11fe686599d9e5fd92c85797f338d12f
marconebrito <marcone.brito@elystech.com.br>
2024-05-30 14:10:52 -0300

    Alteração na responsividade 576px

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById("modal");
-    var modalContent = document.getElementById("modalContent");
-    var closeBtn = document.querySelector(".close");
-    var cards = document.querySelectorAll(".card");
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card');
+    const modal = document.getElementById('modal');
+    const modalContent = document.getElementById('modalContent');
+    const closeModal = document.querySelector('.close');
 
-    cards.forEach(function(card) {
-        card.addEventListener("click", function() {
-            var modalId = card.getAttribute("data-modal");
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", modalId, true);
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    modalContent.innerHTML = xhr.responseText;
-                    modal.style.display = "block";
-
-                    // Adicionar classe para esconder overflow do body
-                    document.body.classList.add("no-scroll");
-                }
-            };
-            xhr.send();
+    cards.forEach(card => {
+        card.addEventListener('click', function () {
+            const modalUrl = this.getAttribute('data-modal');
+            fetch(modalUrl)
+                .then(response => response.text())
+                .then(data => {
+                    modalContent.innerHTML = data;
+                    modal.style.display = 'block';
+                    document.body.classList.add('no-scroll');
+                });
         });
     });
 
-    closeBtn.addEventListener("click", function() {
-        modal.style.display = "none";
+    closeModal.addEventListener('click', function () {
+        modal.style.display = 'none';
+        document.body.classList.remove('no-scroll');
     });
 
-    window.addEventListener("click", function(event) {
+    window.addEventListener('click', function (event) {
         if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    });
-
-    // Remover classe no-scroll após a transição da modal terminar
-    modal.addEventListener("transitionend", function(event) {
-        if (event.propertyName === "opacity" && modal.style.opacity === "0") {
-            document.body.classList.remove("no-scroll");
+            modal.style.display = 'none';
+            document.body.classList.remove('no-scroll');
         }
     });
 });
