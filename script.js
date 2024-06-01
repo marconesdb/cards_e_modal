e76e92b66652b46a5036d5087590b3076f9720fc
marconebrito <marcone.brito@elystech.com.br>
2024-06-01 20:58:05 -0300

    Alteração no Modal

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.card');
-    const modal = document.getElementById('modal');
-    const modalContent = document.getElementById('modalContent');
-    const closeModal = document.querySelector('.close');
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.add('open');
+}
 
-    cards.forEach(card => {
-        card.addEventListener('click', function () {
-            const modalUrl = this.getAttribute('data-modal');
-            fetch(modalUrl)
-                .then(response => response.text())
-                .then(data => {
-                    modalContent.innerHTML = data;
-                    modal.style.display = 'block';
-                    document.body.classList.add('no-scroll');
-                });
-        });
-    });
-
-    closeModal.addEventListener('click', function () {
-        modal.style.display = 'none';
-        document.body.classList.remove('no-scroll');
-    });
-
-    window.addEventListener('click', function (event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-            document.body.classList.remove('no-scroll');
-        }
-    });
-});
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.remove('open');
+}
